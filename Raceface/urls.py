eb86d44c8a12789df8947769a163a4b05f150b81
Craig Pestell <craigpestell@gmail.com>
2014-06-12 21:25:54 -0700

    sorted out url handling, static file loading.

--- Raceface/urls.py
+++ Raceface/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
-admin.autodiscover()
+from Raceface import views
+from django.conf import settings
+from django.conf.urls.static import static
 
+
+admin.autodiscover()
 urlpatterns = patterns('',
+    url(r'^$', views.index),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^(?P<catalog_type>\w+)/', include('catalog.urls')),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
