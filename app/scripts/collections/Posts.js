0afe8e564175796e3b7284806a1a00949725d5c5
Craig Pestell <craigpestell@gmail.com>
2015-09-14 23:41:51 -0400

    switching URLs to prod - will have to add config variables for dev/production

--- app/scripts/collections/Posts.js
+++ app/scripts/collections/Posts.js
@@ -5,7 +5,7 @@ define([
     'models/Post'
 ], function(_, Backbone, PostModel){
     var PostCollection = Backbone.Collection.extend({
-        url: 'http://45.55.25.163:8001/api/posts',
+        url: 'http://10.134.40.148:8001/api/posts',
         model: PostModel
     });
     // You don't usually return a collection instantiated
