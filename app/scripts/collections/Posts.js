1c28e3bf813aa63aa2fb49c733c32f612a7d254c
Craig Pestell <craigpestell@gmail.com>
2015-09-14 23:56:29 -0400

    changing port

--- app/scripts/collections/Posts.js
+++ app/scripts/collections/Posts.js
@@ -5,7 +5,7 @@ define([
     'models/Post'
 ], function(_, Backbone, PostModel){
     var PostCollection = Backbone.Collection.extend({
-        url: 'http://10.134.40.148:8001/api/posts',
+        url: 'http://10.134.40.148:8081/api/posts',
         model: PostModel
     });
     // You don't usually return a collection instantiated
